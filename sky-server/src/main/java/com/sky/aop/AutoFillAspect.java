085a5eb2bba9273a8ee47c8fb87079bf0a68c2ad
Lynns-ui <3199791722@qq.com>
2025-12-05 20:30:58 +0800

    todo aop切面...

---
+++ sky-server/src/main/java/com/sky/aop/AutoFillAspect.java
@@ -0,0 +1,13 @@
+package com.sky.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+@Aspect
+public class AutoFillAspect {
+
+}
--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -1,11 +1,17 @@
 package com.sky.mapper;
 
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.vo.DishVO;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
 
 @Mapper
 public interface DishMapper {
 
     @Select("select count(*) from dish where category_id=#{id}")
     Integer count(Long id);
+
+    List<DishVO> findAll(DishPageQueryDTO dishPageQueryDTO);
 }
